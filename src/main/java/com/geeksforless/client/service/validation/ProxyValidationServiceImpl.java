28e77aac62b8c4974c0ed2619267e1b864d256c3
FedorenkoTaras <tarakhanio84@gmail.com>
2024-04-12 22:25:35 +0300

    add ProxyValidationService and tests

--- src/main/java/com/geeksforless/client/service/validation/ProxyValidationServiceImpl.java
+++ src/main/java/com/geeksforless/client/service/validation/ProxyValidationServiceImpl.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
