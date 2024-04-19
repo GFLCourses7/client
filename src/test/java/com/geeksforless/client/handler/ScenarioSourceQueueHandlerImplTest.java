9fd26d99af921f6bb6ead2cfeb35542b609eef55
TarasFedorenko <119684322+TarasFedorenko@users.noreply.github.com>
2024-04-19 18:44:05 +0300

    add internal api scenarioUpdate endpoint with services

--- src/test/java/com/geeksforless/client/handler/ScenarioSourceQueueHandlerImplTest.java
+++ src/test/java/com/geeksforless/client/handler/ScenarioSourceQueueHandlerImplTest.java
@@ -1,28 +1,36 @@
 package com.geeksforless.client.handler;
 
+import com.geeksforless.client.exception.ScenarioNotFoundException;
 import com.geeksforless.client.handler.impl.ScenarioSourceQueueHandlerImpl;
 import com.geeksforless.client.model.Scenario;
+import com.geeksforless.client.model.ScenarioDto;
+import com.geeksforless.client.repository.ScenarioRepository;
 import com.geeksforless.client.service.Publisher;
 import com.geeksforless.client.service.PublisherImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static java.util.Optional.of;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ScenarioSourceQueueHandlerImplTest {
-
+    @InjectMocks
     private ScenarioSourceQueueHandlerImpl queueHandler;
+
+    @Mock
+    private ScenarioRepository scenarioRepository;
 
     @BeforeEach
     void setUp() {
         Publisher publisher = mock(PublisherImpl.class);
         doNothing().when(publisher).sendMessage();
-        queueHandler = new ScenarioSourceQueueHandlerImpl();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -51,5 +59,41 @@ public class ScenarioSourceQueueHandlerImplTest {
         assertTrue(scenarioOptional.isPresent());
         assertEquals(scenario, scenarioOptional.get());
         assertEquals(0, queueHandler.getQueue().size());
+    }
+
+    @Test
+    public void testUpdateScenario_Success() {
+        ScenarioDto scenarioDto = new ScenarioDto();
+        scenarioDto.setId(1L);
+        scenarioDto.setName("Test");
+        scenarioDto.setResult("Success");
+
+        Scenario scenario = new Scenario();
+        scenario.setId(1L);
+        scenario.setName("Test Scenario");
+        scenario.setResult("Initial");
+        scenario.setDone(false);
+
+
+        when(scenarioRepository.findById(scenarioDto.getId())).thenReturn(of(scenario));
+        when(scenarioRepository.save(any(Scenario.class))).thenReturn(scenario);
+
+        ScenarioDto updatedScenarioDto = queueHandler.updateScenario(scenarioDto);
+
+        assertEquals(scenarioDto.getResult(), updatedScenarioDto.getResult());
+        assertTrue(scenario.isDone());
+    }
+
+    @Test
+    public void testUpdateScenario_ScenarioNotFound() {
+
+        ScenarioDto scenarioDto = new ScenarioDto();
+        scenarioDto.setId(1L);
+
+        when(scenarioRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ScenarioNotFoundException.class, () -> {
+            queueHandler.updateScenario(scenarioDto);
+        });
     }
 }
