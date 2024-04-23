77af84c6f8bb2b6b79b804945f06b057e385d06a
Denys Tolkachov <mrmarkutan@gmail.com>
2024-04-23 11:28:10 +0300

    Add robust error handling logic

--- src/main/java/com/geeksforless/client/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/geeksforless/client/controller/advice/GlobalExceptionHandler.java
@@ -1,46 +1,49 @@
-package com.geeksforless.client.controller.advice;
+//package com.geeksforless.client.controller.advice;
+//
+//import com.geeksforless.client.exception.ScenarioNotFoundException;
+//import com.geeksforless.client.exception.UsernameIsAlreadyExist;
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//    @ExceptionHandler(value = {IllegalArgumentException.class})
+//    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
 
-import com.geeksforless.client.exception.ScenarioNotFoundException;
-import com.geeksforless.client.exception.UsernameIsAlreadyExist;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(value = {IllegalArgumentException.class})
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    @ExceptionHandler(value = {AuthenticationException.class})
-    public ResponseEntity<String> handleAuthenticationException(AuthenticationException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    //Для @Valid
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-
-    @ExceptionHandler(value = {UsernameIsAlreadyExist.class})
-    public ResponseEntity<String> handleUsernameIsAlreadyExist(UsernameIsAlreadyExist ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
-    }
-
-    @ExceptionHandler(value = {UsernameNotFoundException.class})
-    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
-        return ResponseEntity.badRequest().body("User name not found ");
-    }
-
-    @ExceptionHandler(value = {ScenarioNotFoundException.class})
-    public ResponseEntity<String> handleScenarioNotFoundException(ScenarioNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-}
+//    @ExceptionHandler(value = {AuthenticationException.class})
+//    public ResponseEntity<String> handleAuthenticationException(AuthenticationException ex) {
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+//
+//    //Для @Valid
+////    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+////    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+////        return ResponseEntity.badRequest().body(ex.getMessage());
+////    }
+//
+//
+//    @ExceptionHandler(value = {UsernameIsAlreadyExist.class})
+//    public ResponseEntity<String> handleUsernameIsAlreadyExist(UsernameIsAlreadyExist ex) {
+//        return ResponseEntity.badRequest().body(ex.getMessage());
+//    }
+//
+//    @ExceptionHandler(value = {UsernameNotFoundException.class})
+//    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+//        return ResponseEntity.badRequest().body("User name not found ");
+//    }
+//
+//    @ExceptionHandler(value = {ScenarioNotFoundException.class})
+//    public ResponseEntity<String> handleScenarioNotFoundException(ScenarioNotFoundException ex) {
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+//    }
+//}
